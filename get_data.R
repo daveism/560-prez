d373a5e904a8ba0e108099f3a46396890755e28a
Dave Michelson <daveism@gmail.com>
2017-10-07 14:05:09 -0400

    import and clean

--- get_data.R
+++ get_data.R
@@ -7,11 +7,32 @@
 tracks_url_atlantic <- paste0("http://www.aoml.noaa.gov/hrd/hurdat/hurdat2-1851-2016-apr2017.txt")
 tracks_url_pacific <- paste0("http://www.aoml.noaa.gov/hrd/hurdat/hurdat2-nepac-1949-2016-apr2017.txt")
 
+source("/Users/daveism/GitHub/560-prez/clean_data.R")
+
 #import hurricane tracks into hurr_tracks
 hurr_tracks_atlantic <- readLines(tracks_url_atlantic)
-# hurr_tracks_pacific <- readLines(tracks_url_pacific)
+hurr_tracks_pacific <- readLines(tracks_url_pacific)
 
+#Atlantic
 atlantic_meta_data <- create_meta_data(hurr_tracks_atlantic, "Western Atlantic")
 atlantic_obs_data <- create_obs_data(hurr_tracks_atlantic, atlantic_meta_data, "Western Atlantic")
+atlantic_meta_data <- append_meta_data(atlantic_obs_data, atlantic_meta_data)
 
+#Pacific
+pacific_meta_data <- create_meta_data(hurr_tracks_pacific, "Eastern Pacific")
+pacific_obs_data <- create_obs_data(hurr_tracks_pacific, pacific_meta_data, "Eastern Pacific")
+pacific_meta_data <- append_meta_data(pacific_obs_data, pacific_meta_data)
+
+#append datasets
+hurr_meta <- rbind(atlantic_meta_data,pacific_meta_data)
+hurr_obs <- rbind(atlantic_obs_data,pacific_obs_data)
+
+#remove single datasets
+rm(pacific_meta_data)
+rm(pacific_obs_data)
+rm(atlantic_meta_data)
+rm(atlantic_obs_data)
+
+rm(hurr_tracks_atlantic)
+rm(hurr_tracks_pacific)
 # import_hurdat(tracks_url_atlantic, "Atlantic")
