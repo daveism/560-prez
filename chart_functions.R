c2877a74e7e78bb54842039cfdc72c88709dfa89
Dave Michelson <daveism@gmail.com>
2017-10-09 18:17:01 -0400

    fix for chart labels

--- chart_functions.R
+++ chart_functions.R
@@ -3,28 +3,6 @@
 #graphing functions
 #############################
 
-ggScatterSimple <-  function(data, xField, yField, method, title,
-                       xLabel, yLabel, lowerlimit, upperlimit, source){
-
-
-  m <- lm(yField ~ xField, data);
-  r2 <- format(summary(m)$r.squared, digits = 3)
-  pearsons <- format(cor(xField, yField, use = "complete.obs"), digits = 3)
-
-  ggplot(data, aes(x = xField, y = yField)) +
-  geom_point(color="#b2ddf2") +
-  geom_smooth(method = method,color="#008fd5",se=0) +
-  theme_minimal(base_size=9) +
-  coord_cartesian(ylim = c(lowerlimit, upperlimit)) +
-  labs(title= paste(title),
-     subtitle=paste("R-squared = ",r2),
-     x=xLabel,
-     y=yLabel,
-     caption=paste("Source:",source)) +
-     theme(plot.subtitle = element_text(color="#666666"),
-          plot.caption = element_text(color="#AAAAAA", size=6))
-}
-
 ggScatterAuto <-  function(data, xField, yField, method, title,
                        xLabel, yLabel, source){
 
@@ -85,85 +63,6 @@ ggScatterAutoNoRLim <-  function(data, xField, yField, method, title,
           plot.caption = element_text(color="#AAAAAA", size=6))
 }
 
-ggScatterType <-  function(data, xField, yField, method, title,
-                       xLabel, yLabel, lowerlimit, upperlimit, source, type){
-
- #doc and other
- base_size <- ifelse(type == "doc", as.numeric(3) , as.numeric(9) )
- text_size <- ifelse(type == "doc", as.numeric(3) , as.numeric(6) )
- point_outline_size <- ifelse(type == "doc", as.numeric(.1) , as.numeric(1) )
- point_size <- ifelse(type == "doc", as.numeric(.05) , as.numeric(1) )
- line_size <- ifelse(type == "doc", as.numeric(.5) , as.numeric(1) )
-
-
-  m <- lm(yField ~ xField, data);
-  r2 <- format(summary(m)$r.squared, digits = 3)
-  pearsons <- format(cor(xField, yField, use = "complete.obs"), digits = 3)
-
-  ggplot(data, aes(x = xField, y = yField)) +
-  geom_point(size = point_outline_size,colour = "black") +
-  geom_point(colour="#b2ddf2", size =point_size,  alpha = 0.40 ) +
-  geom_smooth(method = method,color="#008fd5", se=0, size=line_size) +
-  theme_minimal(base_size=base_size) +
-  coord_cartesian(ylim = c(lowerlimit, upperlimit)) +
-  labs(title= paste(title),
-      subtitle=paste("R-squared = ",r2),
-       x=xLabel,
-       y=yLabel,
-       caption=paste("Source:",source)) +
-  theme(plot.subtitle = element_text(color="#666666"),
-        plot.caption = element_text(color="#AAAAAA", size=text_size))
-}
-
-
-ggScatterPt <-  function(data, xField, yField, method, title,
-                       xLabel, yLabel, lowerlimit, upperlimit, pt_name,
-                       pt_title, source){
-
-  m <- lm(yField ~ xField, data);
-  r2 <- format(summary(m)$r.squared, digits = 3)
-  pearsons <- format(cor(xField, yField, use = "complete.obs"), digits = 3)
-
-  ggplot(data, aes(x = xField, y = yField)) +
-  geom_point(aes(color = pt_name)) +
-  labs(colour=pt_title) +
-  geom_smooth(method = method,color="#008fd5",se=0) +
-  theme_minimal(base_size=9) +
-  coord_cartesian(ylim = c(lowerlimit, upperlimit)) +
-  labs(title= paste(title),
-    subtitle=paste("R-squared = ",r2),
-     x=xLabel,
-     y=yLabel,
-     caption=paste("Source:",source)) +
-     theme(plot.subtitle = element_text(color="#666666"),
-          plot.caption = element_text(color="#AAAAAA", size=6))
-}
-
-ggScatterTotals <-  function(data, xField, yField, method, title,
-                       xLabel, yLabel, lowerlimit, upperlimit, source){
-
-   max_value <- max(unlist(yField), na.rm = TRUE)
-   min_value <- min(unlist(yField), na.rm = TRUE)
-
-  m <- lm(yField ~ xField, data);
-  r2 <- format(summary(m)$r.squared, digits = 3)
-  pearsons <- format(cor(xField, yField, use = "complete.obs"), digits = 3)
-
-  ggplot(data, aes(x = xField, y = yField)) +
-  geom_point(color="#b2ddf2") +
-  geom_smooth(method = method,color="#008fd5",se=0) +
-  theme_minimal(base_size=9) +
-  coord_cartesian(ylim = c(min_value, max_value)) +
-  labs(title= paste(title),
-    subtitle=paste("R-squared = ",r2),
-     x=xLabel,
-     y=yLabel,
-     caption=paste("Source:",source)) +
-     theme(plot.subtitle = element_text(color="#666666"),
-          plot.caption = element_text(color="#AAAAAA", size=6))
-}
-
-# geom_bar(stat="summary", fun.y="sum")
 
 ggBarTime<- function(data, title, xfield, yfield, xlabel, ylabel, source){
 
@@ -191,18 +90,37 @@ ggBarTime<- function(data, title, xfield, yfield, xlabel, ylabel, source){
            panel.background = element_blank())
 }
 
+makeBreaks <- function(field){
+  max_value <- max(unlist(field), na.rm = TRUE)
+  min_value <- min(unlist(field), na.rm = TRUE)
+
+  diff <- max_value - min_value
+
+  break_value <- diff/10
+
+
+
+}
 
 ggBarMaxAll<- function(data, title, xfield, yfield, xlabel, ylabel, source){
 
   m <- lm(yfield ~ xfield, data);
   r2 <- format(summary(m)$r.squared, digits = 3)
 
+  b_breaks = c(185101.1, 190001.1, 193001.1, 195001.1, 196001.1, 197001.1, 198001.1,  199001.1, 200001.1, 201001.1)
+  b_labels = c("1850",  "1900",   "1930",   "1950",  "1960",   "1970",  "1980",     "1990",    "2000",  "2010")
+
+  #adjust per basin
+  if (str_sub(xfield[1], -1, -1) == 2){
+    b_breaks = c(185101.2, 190001.2, 193001.2, 195001.2, 196001.2, 197001.2, 198001.2,  199001.2, 200001.2, 201001.2)
+    b_labels = c("1850",  "1900",   "1930",   "1950",   "1960",   "1970",   "1980",     "1990",   "2000",  "2010")
+  }
 
   ggplot(data,aes(as.factor(xfield),  yfield)) +
   geom_bar(position = "dodge", stat = "summary", fun.y = "max", fill="#b2ddf2", color="#b2ddf2", alpha = 0.25) +
   scale_x_discrete(
-                  breaks = c(185101.1, 190001.1, 193001.1, 195001.1, 196001.1, 197001.1, 198001.1,  199001.1, 200001.1, 201001.1),
-                  labels = c("1850",  "1900",   "1930",   "1950",  "1960",   "1970",  "1980",     "1990",    "2000",  "2010")) +
+                  breaks = b_breaks,
+                  labels = b_labels) +
   geom_smooth(method = "loess", color="#008fd5", se = FALSE) +
   # , minor_breaks = seq(0, 4.8, 0.1)
   theme_minimal(base_size=9) +
@@ -221,64 +139,5 @@ ggBarMaxAll<- function(data, title, xfield, yfield, xlabel, ylabel, source){
 }
 
 
-ggYearCnt <- function(data, title, xfield, yfield, xlabel, ylabel, source){
 
-  max_value <- as.numeric(max(unlist(xfield), na.rm = TRUE))
-  min_value <- as.numeric(min(unlist(xfield), na.rm = TRUE))
-  print(max_value)
-
-  diff <- max_value - min_value
-
-  ggplot(data, aes(x = xfield)) +
-  geom_histogram(stat = "max", fill="grey", color="dark grey", bins=diff) +
-  theme_minimal(base_size=9) +
-   labs(title= paste(title),
-        subtitle="",
-        x=xlabel,
-        y=ylabel,
-        caption=paste("Source:",source)) +
-        theme(plot.subtitle = element_text(color="#666666"),
-                 plot.caption = element_text(color="#AAAAAA", size=6))
-}
-
-ggYear <- function(data, title, dataname, years, source){
-  ggplot(data, aes(x = dataname)) +
-  geom_histogram(stat = "bin", fill="grey", color="dark grey", bins=years) +
-  theme_minimal(base_size=9) +
-   labs(title= paste(title),
-        subtitle="",
-        x="Year",
-        y="Max wind in MPH",
-        caption=paste("Source:",source)) +
-        theme(plot.subtitle = element_text(color="#666666"),
-                 plot.caption = element_text(color="#AAAAAA", size=6))
-}
-
-ggYearFac <- function(data, dataname, years, facet_name, source){
-  ggplot(data, aes(x = dataname)) +
-  geom_histogram(stat = "bin", fill="grey", color="dark grey", bins=years) +
-  theme_minimal(base_size=9) +
-  labs(title= paste("Events by Year 1950-2017"),
-        subtitle="",
-        x="Year",
-        y="Number of events",
-        caption=paste("Source:",source)) +
-        theme(plot.subtitle = element_text(color="#666666"),
-                 plot.caption = element_text(color="#AAAAAA", size=6))+
-  facet_wrap(reformulate(facet_name), ncol = 5)
-}
-
-ggScatterDigfac2 <-  function(data, xField, yField, xlabel, ylabel, title, facet_name, source){
-  ggplot(data, aes(x = xField, y = yField)) +
-  geom_point(color="#b2ddf2") +
-  geom_smooth(method = 'lm',color="#008fd5",se=0) +
-  ggtitle(title) +theme_minimal(base_size=9) + coord_cartesian(ylim = c(0, 200)) +
-  labs(title= paste(title),
-        subtitle="",
-        x=xlabel,
-        y=ylabel,
-        caption=paste("Source:",source)) +
-        theme(plot.subtitle = element_text(color="#666666"),
-                 plot.caption = element_text(color="#AAAAAA", size=6))+
-  facet_wrap(reformulate(facet_name), ncol = 3)
-}
+  # facet_wrap(reformulate(facet_name), ncol = 5)
