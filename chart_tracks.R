8b71ca5ef580de8fb1b74c9909c4d82161f538d1
Dave Michelson <daveism@gmail.com>
2017-10-11 20:49:16 -0400

    redo

--- chart_tracks.R
+++ chart_tracks.R
@@ -2,8 +2,8 @@
 ###Hurricane winds by storm
 #############################
 
-basin_meta_hurr <- subset(hurr_meta, hurr_meta$basin == "Western Atlantic" & hurr_meta$year >= 2016)
-basin_obs_hurr <- subset(hurr_obs, hurr_obs$basin == "Western Atlantic" & hurr_obs$year >= 2016)
+basin_meta_hurr <- subset(hurr_meta, hurr_meta$basin == "Western Atlantic" & hurr_meta$year >= 2000)
+basin_obs_hurr <- subset(hurr_obs, hurr_obs$basin == "Western Atlantic" & hurr_obs$year >= 2000)
 
 basin_meta_hurr <- arrange(basin_meta_hurr,desc(num_id))
 basin_obs_hurr <- arrange(basin_obs_hurr,desc(num_id))
